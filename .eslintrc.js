9b846816d08700a089001ffa71ff6f8d0c7fd98a
Dani <danizt96@gmail.com>
2022-04-11 23:58:06 +0200

    install and config ESLINT + Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const RULES = {
+  OFF: "off",
+  WARN: "warn",
+  ERROR: "error",
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": RULES.OFF,
+    "react/prop-types": RULES.OFF,
+  },
+}
