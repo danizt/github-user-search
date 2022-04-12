277c64ca28aa951f49ea9f1f89d87e2ebda74b90
Dani <danizt96@gmail.com>
2022-04-12 14:07:41 +0200

    install and config fluentUI

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,13 +1,24 @@
-import { HomeStyle } from './home.jss';
- 
+import { HomeStyle } from "./home.jss"
+import { SearchBox } from "@fluentui/react/lib/SearchBox"
+import { Stack, IStackTokens } from "@fluentui/react/lib/Stack"
 
 export const Home = () => {
-  const homeStyle = HomeStyle();
+  const homeStyle = HomeStyle()
+  const stackTokens: Partial<IStackTokens> = { childrenGap: 20 }
 
   return (
     <>
       <div className="App">
+        {/* Header */}
         <p className={homeStyle.titleContainer}>Hello world!</p>
+        {/* Content */}
+        <Stack tokens={stackTokens}>
+          <SearchBox
+            placeholder="Search"
+            onSearch={(newValue) => console.log("value is " + newValue)}
+          />
+        </Stack>
+        {/* Footer */}
       </div>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import { initializeIcons } from "@fluentui/react/lib/Icons"
+
+initializeIcons()
 
 ReactDOM.render(
   <React.StrictMode>
