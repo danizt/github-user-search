4d4b4b82a4cdd6f34ef07b151ad305c301eadb29
Dani <danizt96@gmail.com>
2022-04-13 22:20:25 +0200

    update GitHubUser model

--- src/services/gitHubService.ts
+++ src/services/gitHubService.ts
@@ -12,9 +12,27 @@ export async function findUserByName(name: string) {
 
   jsonObject.items.forEach((item: any) => {
     users.push({
-      id: item.id,
       login: item.login,
+      id: item.id,
+      nodeId: item.node_id,
+      avatarUrl: item.avatar_url,
+      gravatarId: item.gravatar_id,
+      url: item.url,
+      htmlUrl: item.html_url,
+      followersUrl: item.followers_url,
+      followingUrl: item.following_url,
+      gistsUrl: item.gists_url,
+      starredUrl: item.starred_url,
+      subscriptionsUrl: item.subscriptions_url,
+      organizationsUrl: item.organizations_url,
+      reposUrl: item.repos_url,
+      eventsUrl: item.events_url,
+      receivedEventsUrl: item.received_events_url,
+      type: item.type,
+      siteAdmin: item.site_admin,
+      score: item.score,
     })
   })
+
   return users
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,21 @@
 export interface GitHubUser {
-  id: number
   login: string
+  id: number
+  nodeId: string
+  avatarUrl: string
+  gravatarId: number
+  url: string
+  htmlUrl: string
+  followersUrl: string
+  followingUrl: string
+  gistsUrl: string
+  starredUrl: string
+  subscriptionsUrl: string
+  organizationsUrl: string
+  reposUrl: string
+  eventsUrl: string
+  receivedEventsUrl: string
+  type: string
+  siteAdmin: boolean
+  score: number
 }
